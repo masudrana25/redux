625a9804e3b7bc775a290a56ff7fbebbaf138a9d
masudrana25 <mdmasudrana2512000@gmail.com>
2023-06-06 15:25:14 +0600

    dbhdj

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -1,10 +1,10 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
-export const ROMOVE_FROM_CART = 'ROMOVE_FROM_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 
 export const addToCart = id => {
   return { type: ADD_TO_CART, id };
 };
 
 export const removeFromCart = id => {
-  return { type: ROMOVE_FROM_CART , id };
+  return { type: REMOVE_FROM_CART , id };
 }
